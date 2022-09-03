4a04f33ca0fa4167a0f9bd6c12103e2a662dde6c
Guilherme <111164179+guirque@users.noreply.github.com>
2022-09-03 20:21:39 -0300

    5 segundos
    Agora, os handlers serão buscados após 5 segundos do carregamento da página.

--- json/busca.js
+++ json/busca.js
@@ -1,19 +1,24 @@
 //A cada evento, percorre as postagens e separa handles em um vetor.
-var postagens = Array.from(document.querySelectorAll('article'));
-var handles = [];
-handles = postagens.map((postagem) =>
-{
-    var handle = [...postagem.innerHTML.match(`\@.*?<`).join('')];
-    return handle.slice(0, handle.length - 1).join('');
-});
-
-//Para cada handle, analisa, por meio do Pegabot, se a conta é um bot ou não.
+//A primeira atualização ocorre após 5 segundos
+var postagens;
+var handles;
 var probabilidades = [];
-handles.forEach((handle) =>
-{
-    fetch(`https://backend.pegabot.com.br/botometer?socialnetwork=twitter&profile=${handle}&search_for=profile&limit=1`)
-        .then((retorno) => retorno.json()).then((info) => { probabilidades.push(info.profiles[0].bot_probability.all); console.log("\nESTÁ FUNCIONANDO\n")});
-});
 
-console.log('\n\nSCRIPT RODANDO\n\n');
-console.log(document.querySelector('div'));
+function handlers()
+{
+    var postagens = Array.from(document.querySelectorAll('article'));
+    handles = postagens.map((postagem) =>
+    {
+        var handle = [...postagem.innerHTML.match(`\@.*?<`).join('')];
+        return handle.slice(0, handle.length - 1).join('');
+    });
+
+    //Para cada handle, analisa, por meio do Pegabot, se a conta é um bot ou não.
+    handles.forEach((handle) =>
+    {
+        fetch(`https://backend.pegabot.com.br/botometer?socialnetwork=twitter&profile=${handle}&search_for=profile&limit=1`)
+            .then((retorno) => retorno.json()).then((info) => { probabilidades.push(info.profiles[0].bot_probability.all); console.log("\nESTÁ FUNCIONANDO\n")});
+    });
+}
+
+setTimeout(handlers, 5000);
