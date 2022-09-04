cc8d347d031755122043285c41632794e95f693f
Guilherme <111164179+guirque@users.noreply.github.com>
2022-09-03 23:05:47 -0300

    Update busca.js

--- json/busca.js
+++ json/busca.js
@@ -17,8 +17,31 @@ function handlers()
     handles.forEach((handle) =>
     {
         fetch(`https://backend.pegabot.com.br/botometer?socialnetwork=twitter&profile=${handle}&search_for=profile&limit=1`)
-            .then((retorno) => retorno.json()).then((info) => { probabilidades.push(info.profiles[0].bot_probability.all); console.log("\nESTÁ FUNCIONANDO\n")});
+            .then((retorno) => retorno.json()).then((info, indice) => 
+                                                    { 
+                                                        var chanceDeSerBot = info.profiles[0].bot_probability.all;
+                                                        probabilidades.push(chanceDeSerBot);
+            
+                                                        //O que fazer na página dependendo do resultado
+                                                        if(chanceDeSerBot >= 0.9)
+                                                        {
+                                                            //Ocultar tweet, mas permitindo que o usuário o revele, caso ache necessário
+                                                            document.querySelectorAll('article')[indice].style.display = 'none';   
+                                                        }
+                                                        else if(chanceDeSerBot >= 0.5)
+                                                        {
+                                                            document.querySelectorAll('span')[indice].style.color = 'red';
+                                                        }
+                                                        else
+                                                        {
+                                                            document.querySelectorAll('span')[indice].style.color = 'green';
+                                                        }
+                                                        document.querySelectorAll('span')[indice].innerHTML += ´<div class="extPegaBot"> ${chanceDeSerBot * 100}% de chance de ser bot</div>´;
+                                                    
+                                                    });
     });
+
+
 }
 
-setTimeout(handlers, 5000);
+var updating = setInterval(handlers, 8000);
