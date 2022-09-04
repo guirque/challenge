66beba7108c0f6ca008f345ee5fb853011e2bbe7
Guilherme <111164179+guirque@users.noreply.github.com>
2022-09-04 01:16:19 -0300

    Mais reparos

--- json/busca.js
+++ json/busca.js
@@ -20,30 +20,31 @@ function handlers()
             .then((retorno) => retorno.json()).then((info) => 
                                                     { 
                                                         var chanceDeSerBot = info.profiles[0].bot_probability.all;
-                                                        console.log(`---\nUsuário em Análise: ${handle}\nChance de ser bot: ${chanceDeSerBot}\n---`);
+                                                        //console.log(`---\nUsuário em Análise: ${handle}\nChance de ser bot: ${chanceDeSerBot}\n---`);
                                                         probabilidades.push(chanceDeSerBot);
-                                                        if (!document.querySelectorAll('article')[indice].querySelector('.extPegaBot')){
-                                                        //O que fazer na página dependendo do resultado
-                                                        if(chanceDeSerBot >= 0.9)
-                                                        {
-                                                            //Ocultar tweet, mas permitindo que o usuário o revele, caso ache necessário
-                                                           document.querySelectorAll('article')[indice].querySelector('span').style.display = 'none';   
-                                                        }
-                                                        else if(chanceDeSerBot >= 0.6)
-                                                        {
-                                                            //Probabilidade Alta
-                                                            document.querySelectorAll('article')[indice].querySelector('span').style.color = 'red';
-                                                        }
-                                                        else
-                                                        {
-                                                            //Probabilidade Baixa
-                                                            document.querySelectorAll('article')[indice].querySelector('span').style.color = 'green';
-                                                        }
-                                                        document.querySelectorAll('article')[indice].querySelector('span').innerHTML += '<div class="extPegaBot">' + chanceDeSerBot * 100 + '% de chance de ser bot</div>';
-                                                        }
+                if (!document.querySelectorAll('article')[indice].querySelector('.extPegaBot'))
+                {
+                    //O que fazer na página dependendo do resultado
+                    if (chanceDeSerBot >= 0.9) {
+                        //Ocultar tweet, mas permitindo que o usuário o revele, caso ache necessário
+                        document.querySelectorAll('article')[indice].querySelector('span').style.display = 'none';
+                    }
+                    else if (chanceDeSerBot >= 0.6) {
+                        //Probabilidade Alta
+                        document.querySelectorAll('article')[indice].querySelector('span').style.color = 'red';
+                        document.querySelectorAll('article')[indice].querySelector('span').innerHTML += '<div class="extPegaBot">' + Math.round(chanceDeSerBot * 100) + '% de chance de ser bot (probabilidade alta)</div>';
+                    }
+                    else {
+                        //Probabilidade Baixa
+                        document.querySelectorAll('article')[indice].querySelector('span').style.color = 'green';
+                        document.querySelectorAll('article')[indice].querySelector('span').innerHTML += '<div class="extPegaBot">' + Math.round(chanceDeSerBot * 100) + '% de chance de ser bot (probabilidade baixa)</div>';
+                    }
+
+                }
+                                                    
                                                     });
     });
 }
 
 console.log("\n\nExtensão PegaBot em Execução\n\n");
-var updating = setInterval(handlers, 5000);
+var updating = setInterval(handlers, 8000);
